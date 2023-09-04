22b4aeba19cf19ffa431e89d75b4aef2ae1e70e5
Lassiaa <lassiaa@metropolia.fi>
2023-09-04 15:42:50 +0300

    Mobile done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "raisin-black": "#161925",
+        "delft-blue": "#3A405A",
+        "falu-red": "#81171B",
+        platinum: "#D9D9D9",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
